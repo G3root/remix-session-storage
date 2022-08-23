71e47dc3a2d1f7dd9038ea08d40645cc49164fb2
nafees nazik <nafeesnazik21@gmail.com>
2022-08-23 15:25:32 +0530

    feat: add code extracted from remix-run repo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export const foo = 'foo'
+import { createCookieFactory } from "./cookies"
+import { createCookieSessionStorageFactory } from "./cookieStorage"
+import { createSessionStorageFactory } from "./sessions"
+import { sign, unsign } from "./crypto"
+import { createMemorySessionStorageFactory } from "./memoryStorage"
+
+export * from "cookie"
+export const createCookie = createCookieFactory({ sign: sign, unsign: unsign })
+export const createCookieSessionStorage =
+  createCookieSessionStorageFactory(createCookie)
+export const createSessionStorage = createSessionStorageFactory(createCookie)
+export const createMemorySessionStorage =
+  createMemorySessionStorageFactory(createSessionStorage)
